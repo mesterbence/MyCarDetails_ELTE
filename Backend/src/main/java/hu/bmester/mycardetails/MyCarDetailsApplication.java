21f3d3e18fb7373fb3a6c16ec011a4852a46c733
Mester Bence <mester.bence15@gmail.com>
2022-09-24 19:09:56 +0200

    init backend

---
+++ Backend/src/main/java/hu/bmester/mycardetails/MyCarDetailsApplication.java
@@ -0,0 +1,13 @@
+package hu.bmester.mycardetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCarDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCarDetailsApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/hu/bmester/mycardetails/MyCarDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bmester.mycardetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCarDetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
