f46be1a191fde9372850ea577a409d2a3de5cf1b
Mester Bence <mester.bence15@gmail.com>
2022-09-29 13:46:32 +0200

    test reg

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterComponent } from './register/register.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     MatTableModule,
     MatGridListModule,
-    NgbModule
+    NgbModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
