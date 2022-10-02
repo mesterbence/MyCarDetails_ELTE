510415d828954f88111be77ce8b7f1fe6942aadc
Mester Bence <mester.bence15@gmail.com>
2022-10-02 16:01:42 +0200

    menu bar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycardetails';
+
+  closeResult = '';
+
+  constructor(private offcanvasService: NgbOffcanvas) {}
+
+  open(content: any) {
+    this.offcanvasService.open(content);
+  }
 }
