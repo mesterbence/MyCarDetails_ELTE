12c3e3f5850d6944ab66bcdb2a377eca5851ac72
Mester Bence <mester.bence15@gmail.com>
2022-10-02 19:46:21 +0200

    routing, init reg form

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CostsComponent } from './costs/costs.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'costs', component: CostsComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
