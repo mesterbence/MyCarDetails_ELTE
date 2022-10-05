6b339f85f65f3fa55edcb76abecce674f53b7e06
Mester Bence <mester.bence15@gmail.com>
2022-10-05 22:44:50 +0200

    new cost started

--- Frontend/src/app/cardetails/cardetails.component.ts
+++ Frontend/src/app/cardetails/cardetails.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Car } from '../model/car';
 import { CarService } from '../service/car.service';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cardetails',
@@ -13,11 +14,13 @@ export class CardetailsComponent implements OnInit {
 
   carId!: number;
   carData!: Car;
+  newCostGroup!: FormGroup;
 
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
     private carService: CarService,
-    private modalService: NgbModal) { }
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe(params => {
@@ -32,11 +35,27 @@ export class CardetailsComponent implements OnInit {
         this.router.navigate(['/mycars']);
       }
     });
+    this.newCostGroup = this.formBuilder.group({
+      price: [],
+      mileage: [],
+      title: [''],
+      date: [''],
+      note: ['']
+    })
   }
-  newCost() {
-    console.log(this.carData)
+  onSubmit() {
+    console.log(this.newCostGroup.get('price')?.value,this.newCostGroup.get('mileage')?.value,this.newCostGroup.get('title')?.value,this.newCostGroup.get('date')?.value,this.newCostGroup.get('note')?.value);
+    //this.carService.create(this.newCarForm.get('numberplate')?.value,this.newCarForm.get('brand')?.value,this.newCarForm.get('model')?.value,this.newCarForm.get('fuel')?.value); // típust bevezetni
   }
   open(content: any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+  numberOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
   }
 }
