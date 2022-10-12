86c501680ad8718b8ddaa3185986b6ac4239bcd6
Mester Bence <mester.bence15@gmail.com>
2022-10-12 23:19:34 +0200

    start implementing myprofile

--- Frontend/src/app/myprofile/myprofile.component.ts
+++ Frontend/src/app/myprofile/myprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-myprofile',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyprofileComponent implements OnInit {
 
-  constructor() { }
+  profileGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.profileGroup = this.formBuilder.group({
+      username: [''],
+      password: [''],
+      password2: [''],
+      email: ['']
+    })
+    this.profileGroup.controls['username'].disable();
   }
 
 }
