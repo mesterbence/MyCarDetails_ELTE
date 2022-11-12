e41b0abbb795dfb22916fa2c59a81f1849535fa0
Mester Bence <mester.bence15@gmail.com>
2022-11-12 15:45:29 +0100

    add logo service

---
+++ Frontend/src/app/service/logo.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoService {
+
+  constructor() { }
+  logoUrl: String = "./assets/images/brand-logos/"
+
+  getBrandUrl(brand:String) : String {
+    return this.logoUrl + brand.toLowerCase().replace(' ', '-');
+  }
+}
