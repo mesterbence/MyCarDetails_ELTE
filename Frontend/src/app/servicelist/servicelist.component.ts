b669249da99e36b0acdfde913a26056460055de6
Mester Bence <mester.bence15@gmail.com>
2022-11-12 21:50:27 +0100

    sorting servicelist

--- Frontend/src/app/servicelist/servicelist.component.ts
+++ Frontend/src/app/servicelist/servicelist.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ServiceService} from "../service/service.service";
 import {Service} from "../model/service";
 import {ActivatedRoute} from "@angular/router";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatSort, Sort} from "@angular/material/sort";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
 
 
 @Component({
@@ -13,11 +15,18 @@ import {MatTableDataSource} from "@angular/material/table";
 export class ServicelistComponent implements OnInit {
 
     constructor(private serviceService: ServiceService,
-                private activatedRoute: ActivatedRoute) {
+                private activatedRoute: ActivatedRoute,
+                private _liveAnnouncer: LiveAnnouncer) {
     }
 
     serviceList!: any;
     displayedColumns: string[] = ['done','date', 'mileage', 'note'];
+
+    @ViewChild(MatSort) set matSort(sort: MatSort) {
+        if(this.serviceList) {
+            this.serviceList.sort = sort;
+        }
+    }
 
 
     ngOnInit(): void {
@@ -28,5 +37,16 @@ export class ServicelistComponent implements OnInit {
                 })
             }
         })
+    }
+    announceSortChange(sortState: Sort) {
+        // This example uses English messages. If your application supports
+        // multiple language, you would internationalize these strings.
+        // Furthermore, you can customize the message to add additional
+        // details about the values being sorted.
+        if (sortState.direction) {
+            this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+        } else {
+            this._liveAnnouncer.announce('Sorting cleared');
+        }
     }
 }
