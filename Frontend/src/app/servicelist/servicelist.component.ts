98b501b486781ea91d8d26c6b7ab2014d1636a60
Mester Bence <mester.bence15@gmail.com>
2022-11-12 22:23:47 +0100

    servicelist edit form

--- Frontend/src/app/servicelist/servicelist.component.ts
+++ Frontend/src/app/servicelist/servicelist.component.ts
@@ -5,6 +5,8 @@ import {ActivatedRoute} from "@angular/router";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatSort, Sort} from "@angular/material/sort";
 import {LiveAnnouncer} from "@angular/cdk/a11y";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 
 @Component({
@@ -16,11 +18,13 @@ export class ServicelistComponent implements OnInit {
 
     constructor(private serviceService: ServiceService,
                 private activatedRoute: ActivatedRoute,
-                private _liveAnnouncer: LiveAnnouncer) {
+                private formBuilder: FormBuilder,
+                private modalService: NgbModal) {
     }
 
     serviceList!: any;
     displayedColumns: string[] = ['done','date', 'mileage', 'note'];
+    editServiceFG!: FormGroup;
 
     @ViewChild(MatSort) set matSort(sort: MatSort) {
         if(this.serviceList) {
@@ -38,15 +42,23 @@ export class ServicelistComponent implements OnInit {
             }
         })
     }
-    announceSortChange(sortState: Sort) {
-        // This example uses English messages. If your application supports
-        // multiple language, you would internationalize these strings.
-        // Furthermore, you can customize the message to add additional
-        // details about the values being sorted.
-        if (sortState.direction) {
-            this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-        } else {
-            this._liveAnnouncer.announce('Sorting cleared');
+    clickedService(service: Service,content:any) {
+        this.editServiceFG = this.formBuilder.group({
+            date: service.date,
+            mileage: service.mileage,
+            note: service.note,
+            done: service.done
+        });
+        this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+    }
+    onEditSubmit() {
+        console.log(this.editServiceFG)
+    }
+    numberOnly(event: any): boolean {
+        const charCode = (event.which) ? event.which : event.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
         }
+        return true;
     }
 }
