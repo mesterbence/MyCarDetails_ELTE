3bb0bf4e7543464ccb758066b84c90826a5fff8c
Mester Bence <mester.bence15@gmail.com>
2022-11-20 19:22:33 +0100

    refactor

--- Backend/src/main/java/hu/bmester/mycardetails/service/UserServiceImpl.java
+++ Backend/src/main/java/hu/bmester/mycardetails/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package hu.bmester.mycardetails.service;
 
-import hu.bmester.mycardetails.jwt.JwtUtil;
 import hu.bmester.mycardetails.model.User;
 import hu.bmester.mycardetails.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +16,6 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private JwtUtil jwtUtil;
 
     @Override
     public List<User> findAllUsers() {
@@ -43,16 +39,20 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void changeUserPassword(String password) {
-        User currentUser = jwtUtil.getAuthenticatedUser();
-        if(currentUser == null) { throw new UsernameNotFoundException("Felhasználó nem létezik!"); }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getPrincipal().toString();
+        User currentUser = userRepository.findUserByUsername(currentUserName);
+        if(currentUser == null) { throw new UsernameNotFoundException(currentUserName + " felhasználó nem létezik."); }
         currentUser.setPassword(password);
         userRepository.save(currentUser);
     }
 
     @Override
     public void changeUserMail(String mail) {
-        User currentUser = jwtUtil.getAuthenticatedUser();
-        if(currentUser == null) { throw new UsernameNotFoundException("Felhasználó nem létezik!"); }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getPrincipal().toString();
+        User currentUser = userRepository.findUserByUsername(currentUserName);
+        if(currentUser == null) { throw new UsernameNotFoundException(currentUserName + " felhasználó nem létezik."); }
         currentUser.setEmail(mail);
         userRepository.save(currentUser);
     }
