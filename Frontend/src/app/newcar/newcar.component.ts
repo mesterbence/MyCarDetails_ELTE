53799979022d396b75b5de11ff39229b0cdab808
Mester Bence <mester.bence15@gmail.com>
2022-12-03 21:43:49 +0100

    error if empty fields

--- Frontend/src/app/newcar/newcar.component.ts
+++ Frontend/src/app/newcar/newcar.component.ts
@@ -1,38 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { FuelType } from '../model/fueltype';
-import { CarService } from '../service/car.service';
-import { FueltypeService } from '../service/fueltype.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {FuelType} from '../model/fueltype';
+import {CarService} from '../service/car.service';
+import {FueltypeService} from '../service/fueltype.service';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-newcar',
-  templateUrl: './newcar.component.html',
-  styleUrls: ['./newcar.component.css']
+    selector: 'app-newcar',
+    templateUrl: './newcar.component.html',
+    styleUrls: ['./newcar.component.css']
 })
 export class NewcarComponent implements OnInit {
 
-  fuelTypes!: FuelType[];
-  newCarForm!: FormGroup;
+    fuelTypes!: FuelType[];
+    newCarForm!: FormGroup;
 
-  constructor(private fuelTypeService: FueltypeService,
-    private formBuilder: FormBuilder,
-    private carService: CarService) { }
+    constructor(private fuelTypeService: FueltypeService,
+                private formBuilder: FormBuilder,
+                private carService: CarService,
+                private snackBar: MatSnackBar) {
+    }
 
-  ngOnInit(): void {
-    this.newCarForm = this.formBuilder.group({
-      brand: [''],
-      model: [''],
-      fuel: [''],
-      numberplate: ['']
-    })
+    ngOnInit(): void {
+        this.newCarForm = this.formBuilder.group({
+            brand: [''],
+            model: [''],
+            fuel: [''],
+            numberplate: ['']
+        })
 
-    this.fuelTypeService.getAllFuelTypes().subscribe(
-      data => {
-        this.fuelTypes = data;
-      }
-    );
-  }
-  onSubmit() {
-    this.carService.create(this.newCarForm.get('numberplate')?.value,this.newCarForm.get('brand')?.value,this.newCarForm.get('model')?.value,this.newCarForm.get('fuel')?.value); // típust bevezetni
-  }
+        this.fuelTypeService.getAllFuelTypes().subscribe(
+            data => {
+                this.fuelTypes = data;
+            }
+        );
+    }
+
+    onSubmit() {
+        if (this.newCarForm.get("brand")?.value !== "" &&
+            this.newCarForm.get("model")?.value !== "" &&
+            this.newCarForm.get("fuel")?.value !== "" &&
+            this.newCarForm.get("numberplate")?.value !== "") {
+            this.carService.create(this.newCarForm.get('numberplate')?.value, this.newCarForm.get('brand')?.value, this.newCarForm.get('model')?.value, this.newCarForm.get('fuel')?.value);
+        } else {
+            this.snackBar.open("A mezők kitöltése kötelező!", 'Bezárás', {verticalPosition: 'top', duration: 5000});
+        }
+    }
 }
